a471e5e18d019fbdefdaf8c5adecca523baa95d2
Tellertomate0 <tomteller365@gmail.com>
2025-07-20 21:42:53 +0200

    name exchange, logo exchange

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>{{ config('app.name', 'Panel') }} - @yield('title')</title>
+  <title>{{ config('app.name', 'Redstack Servers') }} - @yield('title')</title>
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <meta name="_token" content="{{ csrf_token() }}">
 
